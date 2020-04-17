d173fa126966d8560ab8bfc33f95d4e08d277b4a
Vladislav Rykov <al373630@uji.es>
2020-04-17 10:50:18 +0200

    pend dao changed

--- dao/pend/pend.py
+++ dao/pend/pend.py
@@ -1,24 +1,4 @@
-import psycopg2
-
-# decorator implementation
-def with_psql(f):
-    def _with_psql(*args, **kwargs):
-        conn = psycopg2.connect('dbname=gateway')
-        cur = conn.cursor()
-
-        try:
-            res = f(cur, *args, **kwargs)
-        except (Exception, psycopg2.DatabaseError) as error:
-            conn.rollback()
-            res = (False, error)
-        else:
-            conn.commit()
-        finally:
-            cur.close()
-            conn.close()
-    
-        return res
-    return _with_psql
+from misc import with_psql
 
 @with_psql
 def create(cur, appkey, devid, msg):
