65d79ff9a06bbe995dc361882bbcfa2b609077b5
Vladislav Rykov <al373630@uji.es>
2020-05-08 07:40:28 -0900

    improved preset

--- preset.sh
+++ preset.sh
@@ -2,9 +2,16 @@ pip3 install virtualenv
 python3 -m virtualenv env
 source env/bin/activate
 
-sudo apt-get install libpq-dev
+sudo apt-get install -y postgresql postgresql-contrib
+sudo apt-get install -y libpq-dev
 
-pip install -r requirements.txt
+pip install -r app/requirements.txt
+
+sudo -u postgres psql -c "CREATE USER ${USER} WITH PASSWORD 'dev';"
+sudo -u postgres psql -c "CREATE DATABASE iotserver WITH OWNER=${USER};"
+psql -d iotserver -f db.sql
+
+mkdir app/app/data
 
 export FLASK_APP=run.py
 export FLASK_ENV=production
