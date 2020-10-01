dfa16f855ea821087da4caf7c8ab16e6c19e32f6
Vladislav Rykov <al373630@uji.es>
2020-10-01 14:37:40 +0200

    recent activity links for admins

--- app/app/views_admin.py
+++ app/app/views_admin.py
@@ -527,7 +527,9 @@ def administration_user_recent_activity(name):
     
     for r in recent_activity:
         dev = dd.get(r[5], r[6])[1]
-        ra += '<tr><th scope="row">'+r[1]+'</th><th>'+r[2]+'</th><th>'+r[0]+'</th><th>'+str(ddm.read_data(r[3].tobytes(), dev[3]))+'</th></tr>'
+        hrefapp = '<th scope="row"><a href="application/{}">{}</a></th>'.format(r[5], r[1])
+        hrefdev = '<th><a href="application/{}/device/{}">{}</a></th>'.format(r[5], r[6], r[2])
+        ra += '<tr>'+hrefapp+hrefdev+'<th>'+r[0]+'</th><th>'+str(ddm.read_data(r[3].tobytes(), dev[3]))+'</th></tr>'
 
     return ra, 200
 
